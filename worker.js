555da4f613f06fdeb94a317a2182382659fd86fb
alibhojani92 <alibhojani92@gmail.com>
2026-01-17 01:51:40 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,12 @@
-
+export default {
+  fetch(request, env, ctx) {
+    return new Response(
+      "GPSC Dental V1 – Worker is LIVE ✅",
+      {
+        headers: {
+          "content-type": "text/plain; charset=UTF-8"
+        }
+      }
+    );
+  }
+};
