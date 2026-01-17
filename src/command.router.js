186875ecf15e1e8d4aa58fe29a4153d70f1538d4
alibhojani92 <alibhojani92@gmail.com>
2026-01-17 13:31:42 +0530

    Delete src directory

--- src/command.router.js
+++
@@ -1,9 +0,0 @@
-import { handleStart } from "./services/user.service";
-
-export async function handleUpdate(update, env) {
-  if (update.message?.text === "/start") {
-    return await handleStart(update, env);
-  }
-
-  return new Response("Ignored 🤷‍♂️");
-}
